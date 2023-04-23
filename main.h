02dfa2f41dfbdc1f5b94c27c5a5315c1049c75a4
Harry Muriithi <hharryjjack@gmail.com>
2023-04-23 12:19:03 -0700

    third set of functions

--- main.h
+++ main.h
@@ -18,5 +18,24 @@ int print_int(va_list types, char buffer[], int flags,
 int print_binary(va_list types, char buffer[], int flags,
 		int width, int precision, int size);
 		/* functions1.c */
+int print_unsigned(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[],
+	int flags, char flag_ch, int width, int precision, int size);
+		/* function2.c */
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_rot13string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
 #endif /* MAIN_H */
